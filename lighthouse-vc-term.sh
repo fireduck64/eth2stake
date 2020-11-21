487b43bc46aa1fc998b10a1456a1db9204663847
nerd <nerd@eth2.1209k.com>
2020-11-20 23:13:45 -0800

    docs

--- lighthouse-vc-term.sh
+++ lighthouse-vc-term.sh
@@ -1,4 +1,5 @@
 
+# run lighthouse vc in ther terminal so you can see it
 mkdir -p ~/data/lighthouse
 
 docker run --rm --name lighthouse-v --network host -v ~/data/lighthouse:/root/.lighthouse sigp/lighthouse lighthouse vc --testnet mainnet --http --graffiti="Fireduck"
