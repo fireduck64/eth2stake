5a47916f72d285477ec55f84ec1b1448581fb202
Joseph Gleason <fireduck@gmail.com>
2020-11-22 14:35:41 -0800

    curl silent

--- geth-height.sh
+++ geth-height.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://localhost:8545/|jq .
+curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://localhost:8545/|jq .
 
