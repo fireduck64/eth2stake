9c8134d502da99f9a444484401a1334e3b8a9320
Joseph Gleason <fireduck@gmail.com>
2020-11-24 11:37:52 -0800

    Some metrics work

---
+++ grafana-start.sh
@@ -0,0 +1,7 @@
+
+docker pull grafana/grafana
+
+docker volume create grafana_db_vol
+
+docker run --rm --network host --name grafana \
+  --mount 'type=volume,src=grafana_db_vol,dst=/var/lib/grafana' grafana/grafana
