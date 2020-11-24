6d5e86f658992e7911da58c03afd6becabd966d6
Joseph Gleason <fireduck@gmail.com>
2020-11-24 11:37:26 -0800

    Fix vc consistency

--- lighthouse-vc-start.sh
+++ lighthouse-vc-start.sh
@@ -7,6 +7,6 @@ docker volume create lighthouse_vc_vol
 
 docker run  -d --restart always --name lighthouse-vc --network host \
   --mount 'type=volume,src=lighthouse_vc_vol,dst=/root/.lighthouse' \
-  $DOCKER_LIGHTHOUSE lighthouse vc --network mainnet --http --graffiti="Fireduck"
+  $DOCKER_LIGHTHOUSE lighthouse vc --network mainnet --http --graffiti="$GRAFFITI"
 
 
--- lighthouse-vc-term.sh
+++ lighthouse-vc-term.sh
@@ -8,7 +8,7 @@ echo $GRAFFITI
 
 docker volume create lighthouse_vc_vol
 
-docker run --rm --name lighthouse-v --network host \
+docker run --rm --name lighthouse-vc --network host \
   --mount 'type=volume,src=lighthouse_vc_vol,dst=/root/.lighthouse' \
   $DOCKER_LIGHTHOUSE lighthouse vc --network mainnet --http --graffiti="$GRAFFITI"
 
