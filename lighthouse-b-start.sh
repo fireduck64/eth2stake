8b9145e7640131ff218f759e07aa6b56ca66a87d
Joseph Gleason <fireduck@gmail.com>
2020-11-25 07:03:40 -0800

    unslash

--- lighthouse-b-start.sh
+++ lighthouse-b-start.sh
@@ -9,4 +9,4 @@ docker volume create lighthouse_b_vol
 docker run -d --restart always --name lighthouse-b --network host \
   --mount 'type=volume,src=lighthouse_b_vol,dst=/root/.lighthouse' \
   $DOCKER_LIGHTHOUSE lighthouse b --network mainnet --http --metrics \
-  --eth1 --eth1-endpoint http://localhost:8545 --slasher 
+  --eth1 --eth1-endpoint http://localhost:8545 
