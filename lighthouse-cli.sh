23c190d9abe7ebe2d6829b674a60a6b99475ff9b
Joseph Gleason <fireduck@gmail.com>
2020-12-23 20:18:32 -0800

    cli should use configured version

--- lighthouse-cli.sh
+++ lighthouse-cli.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
+. eth2-settings.sh
 
 docker volume create lighthouse_vc_vol
 
 docker run -it --rm -v ~/data/validator_keys:/root/validator_keys \
   --mount 'type=volume,src=lighthouse_vc_vol,dst=/root/.lighthouse' \
-  sigp/lighthouse lighthouse $*
+  $DOCKER_LIGHTHOUSE lighthouse $*
 
