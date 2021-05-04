0a2b3f847f30d3d8fb7acf56583e0985471dffbc
Joseph Gleason <fireduck@gmail.com>
2021-05-03 20:32:15 -0700

    Add listen on ipv4

--- lighthouse-b-start.sh
+++ lighthouse-b-start.sh
@@ -9,4 +9,4 @@ docker volume create lighthouse_b_vol
 docker run -d --restart always --name lighthouse-b --network host \
   --mount 'type=volume,src=lighthouse_b_vol,dst=/root/.lighthouse' \
   $DOCKER_LIGHTHOUSE lighthouse b --network mainnet --http --metrics \
-  --eth1 --eth1-endpoints $ETH1_ENDPOINTS
+  --http-address 0.0.0.0 --eth1 --eth1-endpoints $ETH1_ENDPOINTS
