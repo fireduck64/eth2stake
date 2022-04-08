953b5d29e42f1b2c64e81b2aae49bfbfb4574aa4
Joseph Gleason <fireduck@gmail.com>
2022-04-08 10:03:55 -0700

    Allow ipv6 connections

--- lighthouse-b-start.sh
+++ lighthouse-b-start.sh
@@ -9,5 +9,6 @@ docker volume create lighthouse_b_vol
 docker run -d --restart always --name lighthouse-b --network host \
   --mount 'type=volume,src=lighthouse_b_vol,dst=/root/.lighthouse' \
   $DOCKER_LIGHTHOUSE lighthouse b --network mainnet --http --metrics \
+  --http-address :: \
   --validator-monitor-auto \
   --eth1 --eth1-endpoints $ETH1_ENDPOINTS
