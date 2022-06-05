4baac4ed9276ba8f31c07ee8b23d917899346597
Joseph Gleason <fireduck@gmail.com>
2022-06-04 23:53:21 -0700

    Apparently not yet

--- lighthouse-b-start.sh
+++ lighthouse-b-start.sh
@@ -6,10 +6,10 @@ echo "Starting lighthouse beacon"
 
 docker volume create lighthouse_b_vol
 
+# --suggested-fee-recipient $FEE_RECIPIENT \
 docker run -d --restart always --name lighthouse-b --network host \
   --mount 'type=volume,src=lighthouse_b_vol,dst=/root/.lighthouse' \
   $DOCKER_LIGHTHOUSE lighthouse b --network mainnet --http --metrics \
-  ----suggested-fee-recipient $FEE_RECIPIENT \
   --http-address :: \
   --validator-monitor-auto \
   $BEACON_OPTS \
