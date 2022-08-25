2688a31fe1a656364152c463e147f60516d7e903
fireduck64 <fireduck@gmail.com>
2022-08-25 08:26:00 -0700

    Have geth use secret

--- geth-start.sh
+++ geth-start.sh
@@ -10,7 +10,9 @@ docker volume create geth_vol
 
 docker run -d --restart always --name geth \
           --mount 'type=volume,src=geth_vol,dst=/root/.ethereum' \
+          --mount 'type=volume,src=eth_secret_vol,dst=/secrets' \
            --network host \
            $DOCKER_GETH --http --http.addr 0.0.0.0 --http.vhosts '*' \
-           --authrpc.addr 0.0.0.0 --authrpc.port 8551 --authrpc.vhosts '*'
+           --authrpc.addr 0.0.0.0 --authrpc.port 8551 --authrpc.vhosts '*' \
+           --authrpc.jwtsecret /secrets/jwtsecret 
 
