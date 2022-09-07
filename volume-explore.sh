52a73bccfcfc8942bec954020f6dc4c5c3ffb4a6
fireduck64 <fireduck@gmail.com>
2022-09-07 12:20:59 -0700

    Add nthermind to volume list

--- volume-explore.sh
+++ volume-explore.sh
@@ -9,6 +9,7 @@ echo "Your volumes will be mounted in /vol"
 docker run -it --rm \
   -v ~/data:/vol/data \
   --mount 'type=volume,src=geth_vol,dst=/vol/geth' \
+  --mount 'type=volume,src=nethermind_vol,dst=/vol/nethermind' \
   --mount 'type=volume,src=lighthouse_b_vol,dst=/vol/lighthouse_b' \
   --mount 'type=volume,src=lighthouse_vc_vol,dst=/vol/lighthouse_vc' \
   --mount 'type=volume,src=eth_secret_vol,dst=/vol/secrets' \
