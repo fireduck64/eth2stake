f9a8467b99c5b2d95f69edab60da46031f46511b
Joseph Gleason <fireduck@gmail.com>
2022-09-07 11:45:45 -0700

    Add suggested fee to vc

--- lighthouse-vc-start.sh
+++ lighthouse-vc-start.sh
@@ -12,11 +12,11 @@ fi
 
 docker volume create lighthouse_vc_vol
 
-# --suggested-fee-recipient $FEE_RECIPIENT \
 docker run  -d --restart always --name lighthouse-vc --network host \
   --mount 'type=volume,src=lighthouse_vc_vol,dst=/root/.lighthouse' \
   -v ~/data:/vol/data $CLOUDLOCK_SETTINGS \
   $DOCKER_LIGHTHOUSE $CLOUDLOCK_COMMAND lighthouse vc --network mainnet --metrics \
   --beacon-nodes $BEACON_ENDPOINTS \
+  --suggested-fee-recipient $FEE_RECIPIENT \
   $GRAFFITI_SETTINGS
 
