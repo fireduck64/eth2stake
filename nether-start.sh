76ce02c1c0cdc64cf03d68b5d0f524d4bbc33d17
Joseph Gleason <fireduck@gmail.com>
2022-12-07 17:31:13 -0800

    Update nethermind volumes

--- nether-start.sh
+++ nether-start.sh
@@ -7,12 +7,15 @@ echo "Starting nethermind"
 docker volume create nethermind_vol
 
 docker run -d --restart always --name nethermind \
-          --mount 'type=volume,src=nethermind_vol,dst=/nethermind/data' \
+          --mount 'type=volume,src=nethermind_db_vol,dst=/nethermind/nethermind_db' \
+          --mount 'type=volume,src=nethermind_log_vol,dst=/nethermind/logs' \
+          --mount 'type=volume,src=nethermind_keystore_vol,dst=/nethermind/keystore' \
           --mount 'type=volume,src=eth_secret_vol,dst=/secrets' \
            --network host \
-           $DOCKER_NETHERMIND --datadir data \
-           --JsonRpc.JwtSecretFile /secrets/jwtsecret \
+           $DOCKER_NETHERMIND \
            --Network.MaxActivePeers 256 \
            --HealthChecks.Enabled true \
+           --JsonRpc.JwtSecretFile /secrets/jwtsecret \
            --JsonRpc.Port 8545 --JsonRpc.Host '*' \
            --JsonRpc.EnginePort 8551 --JsonRpc.EngineHost '*'
+
--- volume-explore.sh
+++ volume-explore.sh
@@ -9,7 +9,8 @@ echo "Your volumes will be mounted in /vol"
 docker run -it --rm \
   -v ~/data:/vol/data \
   --mount 'type=volume,src=geth_vol,dst=/vol/geth' \
-  --mount 'type=volume,src=nethermind_vol,dst=/vol/nethermind' \
+  --mount 'type=volume,src=nethermind_db_vol,dst=/vol/nethermind_db' \
+  --mount 'type=volume,src=nethermind_log_vol,dst=/vol/nethermind_log' \
   --mount 'type=volume,src=lighthouse_b_vol,dst=/vol/lighthouse_b' \
   --mount 'type=volume,src=lighthouse_vc_vol,dst=/vol/lighthouse_vc' \
   --mount 'type=volume,src=eth_secret_vol,dst=/vol/secrets' \
